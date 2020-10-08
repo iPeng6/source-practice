34e0069f98750f543bb075d34caf37693ba10514
yuliang.peng <ipeng6@qq.com>
2020-10-08 15:45:52 +0800

    vuex

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div @click="$store.commit('add')">count:{{ $store.state.count }}</div>
+    <div @click="$store.dispatch('add')">count:{{ $store.state.count }}</div>
+
+    <div>doubleCount: {{ $store.getters.doubleCount }}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
---
+++ vuex/src/vuex/index.js
@@ -0,0 +1,55 @@
+let Vue
+
+class Store {
+  constructor(options) {
+    this.state = options.state
+    this.mutations = options.mutations
+    this.actions = options.actions
+    this.getters = {}
+
+    const computed = {}
+
+    Object.keys(options.getters).forEach((key) => {
+      computed[key] = () => {
+        return options.getters[key](this.state)
+      }
+
+      Object.defineProperty(this.getters, key, {
+        get: () => this._vm[key],
+      })
+    })
+
+    this._vm = new Vue({
+      data: {
+        state: options.state,
+      },
+      computed,
+    })
+
+    this.commit = this.commit.bind(this)
+    this.dispatch = this.dispatch.bind(this)
+  }
+  commit(type, payload) {
+    this.mutations[type](this.state, payload)
+  }
+  dispatch(type, payload) {
+    this.actions[type](this, payload)
+  }
+}
+
+function install(_Vue) {
+  Vue = _Vue
+
+  Vue.mixin({
+    beforeCreate() {
+      if (this.$options.store) {
+        Vue.prototype.$store = this.$options.store
+      }
+    },
+  })
+}
+
+export default {
+  Store,
+  install,
+}
